5a44f7779571ee41d72764c91a7c5a62c7da3ee9
Luke Towers <luke@luketowers.ca>
2019-05-09 19:09:25 -0600

    Initial commit

---
+++ helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if (! function_exists('auth')) {
+    /**
+     * Get the available auth instance.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    function auth()
+    {
+        return \Backend\Classes\AuthManager::instance();
+    }
+}
