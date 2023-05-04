5f9150adffca50f824d9b93f99847ca1b7149222
Engels Baca Sevilla <engels@engveloper.com>
2023-05-04 14:22:57 -0600

    fix: update callback url

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@
             'config' => [
                 'client_id' => env('AZURE_AD_CLIENT_ID', ''),
                 'client_secret' => env('AZURE_AD_CLIENT_SECRET', ''),
-                'redirect' => Request::root() . '/luketowers/azureadsso/login/microsoft/callback',
+                'redirect' => url('/luketowers/azureadsso/login/microsoft/callback'),
                 'tenant' => env('AZURE_TENANT_ID', 'consumers'),
                 'cms_role_code' => false,
             ]
